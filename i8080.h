dbe7e8b48d1c48735ba6d84bc0627463eb6daa97
Nicolas Allemand <contact@nicolasallemand.com>
2020-05-26 18:11:52 +0200

    added clang-format configuration + added an argument "print_disassembly" to i8080_debug_output

--- i8080.h
+++ i8080.h
@@ -6,29 +6,29 @@
 #include <stdbool.h>
 
 typedef struct i8080 {
-    // memory + io interface
-    void* userdata; // user custom pointer
-    uint8_t (*read_byte)(void*, uint16_t); // user function to read from memory
-    void (*write_byte)(void*, uint16_t, uint8_t); // same for writing to memory
-    uint8_t (*port_in)(void*, uint8_t); // user function to read from port
-    void (*port_out)(void*, uint8_t, uint8_t); // same for writing to port
+  // memory + io interface
+  uint8_t (*read_byte)(void*, uint16_t); // user function to read from memory
+  void (*write_byte)(void*, uint16_t, uint8_t); // same for writing to memory
+  uint8_t (*port_in)(void*, uint8_t); // user function to read from port
+  void (*port_out)(void*, uint8_t, uint8_t); // same for writing to port
+  void* userdata; // user custom pointer
 
-    unsigned long cyc; // cycle count
+  unsigned long cyc; // cycle count
 
-    uint16_t pc, sp; // program counter, stack pointer
-    uint8_t a, b, c, d, e, h, l; // registers
-    // flags: sign, zero, half-carry, parity, carry, interrupt flip-flop
-    bool sf : 1, zf : 1, hf : 1, pf : 1, cf : 1, iff : 1;
-    bool halted : 1;
+  uint16_t pc, sp; // program counter, stack pointer
+  uint8_t a, b, c, d, e, h, l; // registers
+  // flags: sign, zero, half-carry, parity, carry, interrupt flip-flop
+  bool sf : 1, zf : 1, hf : 1, pf : 1, cf : 1, iff : 1;
+  bool halted : 1;
 
-    bool interrupt_pending : 1;
-    uint8_t interrupt_vector;
-    uint8_t interrupt_delay;
+  bool interrupt_pending : 1;
+  uint8_t interrupt_vector;
+  uint8_t interrupt_delay;
 } i8080;
 
 void i8080_init(i8080* const c);
 void i8080_step(i8080* const c);
 void i8080_interrupt(i8080* const c, uint8_t opcode);
-void i8080_debug_output(i8080* const c);
+void i8080_debug_output(i8080* const c, bool print_disassembly);
 
 #endif // I8080_I8080_H_
--- i8080_tests.c
+++ i8080_tests.c
@@ -12,127 +12,123 @@
 static uint8_t* memory = NULL;
 static bool test_finished = 0;
 
-static uint8_t rb(void* userdata, const uint16_t addr) {
-    return memory[addr];
+static uint8_t rb(void* userdata, uint16_t addr) {
+  return memory[addr];
 }
 
-static void wb(void* userdata, const uint16_t addr, const uint8_t val) {
-    memory[addr] = val;
+static void wb(void* userdata, uint16_t addr, uint8_t val) {
+  memory[addr] = val;
 }
 
 static uint8_t port_in(void* userdata, uint8_t port) {
-    i8080* const c = (i8080*) userdata;
+  i8080* const c = (i8080*) userdata;
 
-    uint8_t operation = c->c;
+  uint8_t operation = c->c;
 
-    // print a character stored in E
-    if (operation == 2) {
-        printf("%c", c->e);
-    }
-    // print from memory at (DE) until '$' char
-    else if (operation == 9) {
-        uint16_t addr = (c->d << 8) | c->e;
-        do {
-            printf("%c", rb(c, addr++));
-        } while(rb(c, addr) != '$');
-    }
+  if (operation == 2) { // print a character stored in E
+    printf("%c", c->e);
+  } else if (operation == 9) { // print from memory at (DE) until '$' char
+    uint16_t addr = (c->d << 8) | c->e;
+    do {
+      printf("%c", rb(c, addr++));
+    } while (rb(c, addr) != '$');
+  }
 
-    return 0xFF;
+  return 0xFF;
 }
 
 static void port_out(void* userdata, uint8_t port, uint8_t value) {
-    test_finished = 1;
+  test_finished = 1;
 }
 
+static inline int load_file(const char* filename, uint16_t addr) {
+  FILE* f = fopen(filename, "rb");
+  if (f == NULL) {
+    fprintf(stderr, "error: can't open file '%s'.\n", filename);
+    return 1;
+  }
 
-static int load_file(const char* filename, uint16_t addr) {
-    FILE* f = fopen(filename, "rb");
-    if (f == NULL) {
-        fprintf(stderr, "error: can't open file '%s'.\n", filename);
-        return 1;
-    }
+  // file size check:
+  fseek(f, 0, SEEK_END);
+  size_t file_size = ftell(f);
+  rewind(f);
 
-    // file size check:
-    fseek(f, 0, SEEK_END);
-    size_t file_size = ftell(f);
-    rewind(f);
+  if (file_size + addr >= MEMORY_SIZE) {
+    fprintf(stderr, "error: file %s can't fit in memory.\n", filename);
+    return 1;
+  }
 
-    if (file_size + addr >= MEMORY_SIZE) {
-        fprintf(stderr, "error: file %s can't fit in memory.\n", filename);
-        return 1;
-    }
+  // copying the bytes in memory:
+  size_t result = fread(&memory[addr], sizeof(uint8_t), file_size, f);
+  if (result != file_size) {
+    fprintf(stderr, "error: while reading file '%s'\n", filename);
+    return 1;
+  }
 
-    // copying the bytes in memory:
-    size_t result = fread(&memory[addr], sizeof(uint8_t), file_size, f);
-    if (result != file_size) {
-        fprintf(stderr, "error: while reading file '%s'\n", filename);
-        return 1;
-    }
-
-    fclose(f);
-    return 0;
+  fclose(f);
+  return 0;
 }
 
 // runs a program, handling CALL 5 call to output test results to standard
 // output
-static void run_test(i8080* const c, const char* filename,
-                     unsigned long cyc_expected) {
-    i8080_init(c);
-    c->userdata = c;
-    c->read_byte = rb;
-    c->write_byte = wb;
-    c->port_in = port_in;
-    c->port_out = port_out;
-    memset(memory, 0, MEMORY_SIZE);
+static inline void run_test(
+    i8080* const c, const char* filename, unsigned long cyc_expected) {
+  i8080_init(c);
+  c->userdata = c;
+  c->read_byte = rb;
+  c->write_byte = wb;
+  c->port_in = port_in;
+  c->port_out = port_out;
+  memset(memory, 0, MEMORY_SIZE);
 
-    if (load_file(filename, 0x100) != 0) {
-        return;
-    }
-    printf("*** TEST: %s\n", filename);
+  if (load_file(filename, 0x100) != 0) {
+    return;
+  }
+  printf("*** TEST: %s\n", filename);
 
-    c->pc = 0x100;
-    // inject "out 1,a" at 0x0000 (signal to stop the test)
-    memory[0x0000] = 0xD3;
-    memory[0x0001] = 0x00;
+  c->pc = 0x100;
 
-    // inject "in a,0" at 0x0005 (signal to output some characters)
-    memory[0x0005] = 0xDB;
-    memory[0x0006] = 0x00;
-    memory[0x0007] = 0xC9;
+  // inject "out 1,a" at 0x0000 (signal to stop the test)
+  memory[0x0000] = 0xD3;
+  memory[0x0001] = 0x00;
 
-    long nb_instructions = 0;
+  // inject "in a,0" at 0x0005 (signal to output some characters)
+  memory[0x0005] = 0xDB;
+  memory[0x0006] = 0x00;
+  memory[0x0007] = 0xC9;
 
-    test_finished = 0;
-    while (!test_finished) {
-        nb_instructions += 1;
+  long nb_instructions = 0;
 
-        // uncomment following line to have a debug output of machine state
-        // warning: will output multiple GB of data for the whole test suite
-        // i8080_debug_output(c);
+  test_finished = 0;
+  while (!test_finished) {
+    nb_instructions += 1;
 
-        i8080_step(c);
-    }
+    // uncomment following line to have a debug output of machine state
+    // warning: will output multiple GB of data for the whole test suite
+    // i8080_debug_output(c, false);
 
-    long long diff = cyc_expected - c->cyc;
-    printf("\n*** %lu instructions executed on %lu cycles"
-        " (expected=%lu, diff=%lld)\n\n",
-        nb_instructions, c->cyc,
-        cyc_expected, diff);
+    i8080_step(c);
+  }
+
+  long long diff = cyc_expected - c->cyc;
+  printf("\n*** %lu instructions executed on %lu cycles"
+         " (expected=%lu, diff=%lld)\n\n",
+      nb_instructions, c->cyc, cyc_expected, diff);
 }
 
 int main(void) {
-    memory = malloc(MEMORY_SIZE);
-    if (memory == NULL) {
-        return 1;
-    }
+  memory = malloc(MEMORY_SIZE);
+  if (memory == NULL) {
+    return 1;
+  }
 
-    i8080 cpu;
-    run_test(&cpu, "cpu_tests/TST8080.COM", 4924LU);
-    run_test(&cpu, "cpu_tests/CPUTEST.COM", 255653383LU);
-    run_test(&cpu, "cpu_tests/8080PRE.COM", 7817LU);
-    run_test(&cpu, "cpu_tests/8080EXM.COM", 23803381171LU);
+  i8080 cpu;
+  run_test(&cpu, "cpu_tests/TST8080.com", 4924LU);
+  run_test(&cpu, "cpu_tests/CPUTEST.com", 255653383LU);
+  run_test(&cpu, "cpu_tests/8080PRE.com", 7817LU);
+  run_test(&cpu, "cpu_tests/8080EXM.com", 23803381171LU);
 
-    free(memory);
+  free(memory);
 
-    return 0;
+  return 0;
 }
